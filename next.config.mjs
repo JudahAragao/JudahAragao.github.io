0f89fbcf3b06d1a5fc2916acf97c7c219098a14d
judaharagao <judah.aragao@gmail.com>
2024-03-14 23:43:49 -0300

    output in config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         domains: ['www.ifs.edu.br', 'sergipetec.org.br', 'scontent.faju1-1.fna.fbcdn.net'],
     },
